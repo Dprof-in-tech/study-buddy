8d50203f72b53c949077d1856e4fe4eec6e13576
Isaac Onyemaechi <amaechiisaac450@gmail.com>
2025-04-18 18:23:51 +0100

    little changes

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require('next-pwa')({
   dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
+  //disable: process.env.NODE_ENV === 'development',
   register: true,
   skipWaiting: true,
   // Specify the offline page
