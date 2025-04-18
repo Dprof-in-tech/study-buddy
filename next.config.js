86b4f4854435d2f007f07469d34aee4cd91bd0ad
Isaac Onyemaechi <amaechiisaac450@gmail.com>
2025-04-18 17:11:19 +0100

    fix build error

---
+++ next.config.js
@@ -0,0 +1,19 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+/** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+  // Specify the offline page
+  fallbacks: {
+    document: '/offline'
+  }
+});
+
+const nextConfig = {
+  /* your existing config options here */
+  reactStrictMode: true,
+};
+
+module.exports = withPWA(nextConfig);
